f14e24332d147f0dfa1ee10a075cc0798aa18de4
yoniShewa <yonatanmekuriya@gmail.com>
2024-03-27 09:33:35 +0300

    I have added search feature

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.query, name="query"),
     path("", views.index, name="index"),
     path("encyclopedia/<str:TITLE>/", views.encyclopedia, name="encyclopedia"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import markdown2
-
+import re
 
 from . import util
 
@@ -15,13 +15,44 @@ def index(request):
 def encyclopedia(request, TITLE):
     content = util.get_entry(TITLE)
 
-    # Check if content is None
     if content is None:
-
-        return render(request, "encyclopedia/pages.html",
-                      {"title": TITLE, "content": html_content})
+        return render(request, "encyclopedia/pageNotFound.html")
 
     html_content = markdown2.markdown(content)
 
     return render(request, "encyclopedia/pages.html",
                   {"title": TITLE, "content": html_content})
+
+
+def query(request):
+    title = request.GET.get('q')
+
+    if title is None:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
+
+    content = util.get_entry(title)
+    contents = []
+
+    if content is None:
+        entryList = util.list_entries()
+
+        pattern = re.compile(re.escape(title), re.IGNORECASE)
+
+        for entry in entryList:
+            if pattern.search(entry):
+                contents.append(entry)
+
+        if contents:
+            return render(request, "encyclopedia/search.html", {
+                "content": contents,
+                "title": title
+            })
+        return render(request, "encyclopedia/search.html", {
+            "content": contents,
+            "title": title
+        })
+
+    return render(request, "encyclopedia/pages.html",
+                  {"title": title, "content": content})
